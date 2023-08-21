0681cf0a5dcbb4e974f49544c7188a6bb0842263
wolf3 <wolf3976@gmail.com>
2023-08-20 19:57:14 -0500

    Template files with framework fixes

---
+++ src/main/java/com/utp/desktopAppSchool/DesktopAppSchoolApplication.java
@@ -0,0 +1,11 @@
+package com.utp.desktopAppSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DesktopAppSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesktopAppSchoolApplication.class, args);
+    }
+}
