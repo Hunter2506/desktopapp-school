d9576a7083e56c222d5393f2dbb3c1db17b2cf0b
wolf3 <wolf3976@gmail.com>
2023-08-20 19:48:45 -0500

    Template files with framework

---
+++ src/test/java/com/utp/proyectoIntegrador/ProyectoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.proyectoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesktopAppSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
